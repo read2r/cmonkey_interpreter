8fae33dfd47911dc75c1b58797c9ff4fd991c5b2
read2r <read2r@gmail.com>
2021-12-18 00:31:08 +0900

    Add testing nextToken

--- src/test.c
+++ src/test.c
@@ -1,43 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "token.h"
 #include "lexer.h"
 
+typedef struct _TestToken {
+    TokenType expectedType;
+    char* expectedLiteral;
+} TestToken;
+
 typedef struct _TestList {
     int len;
-    Token* arr[100];
+    TestToken* arr[100];
 } TestList;
 
-typedef Token TestToken;
+TestToken* newTestToken(TokenType tokenType, const char* literal) {
+    TestToken* tt = (TestToken*)malloc(sizeof(TestToken));
+    tt->expectedType = tokenType;
+    tt->expectedLiteral = (char*)malloc(sizeof(char) * (strlen(literal) + 1));
+    memcpy(tt->expectedLiteral, literal, sizeof(char) * (strlen(literal) + 1));
+    return tt;
+}
 
 void appendTest(TestList* tl, TestToken* tt) {
     tl->arr[tl->len] = tt;
     tl->len++;
 }
 
+TestToken* getTestAt(TestList* tl, int idx) {
+    return tl->arr[idx];
+}
+
 void TestNextToken1() {
     char* input = "=+(){},;";
 
     TestList* tests = (TestList*)malloc(sizeof(TestList));
-    appendTest(tests, newToken(TOKEN_ASSIGN, '='));
-    appendTest(tests, newToken(TOKEN_PLUS, '+'));
-    appendTest(tests, newToken(TOKEN_LPAREN, '{'));
-    appendTest(tests, newToken(TOKEN_RPAREN, '}'));
-    appendTest(tests, newToken(TOKEN_LBRACE, '{'));
-    appendTest(tests, newToken(TOKEN_RBRACE, '}'));
-    appendTest(tests, newToken(TOKEN_COMMA, ','));
-    appendTest(tests, newToken(TOKEN_SEMICOLON, ';'));
+    appendTest(tests, newTestToken(TOKEN_ASSIGN, "="));
+    appendTest(tests, newTestToken(TOKEN_PLUS, "+"));
+    appendTest(tests, newTestToken(TOKEN_LPAREN, "("));
+    appendTest(tests, newTestToken(TOKEN_RPAREN, ")"));
+    appendTest(tests, newTestToken(TOKEN_LBRACE, "{"));
+    appendTest(tests, newTestToken(TOKEN_RBRACE, "}"));
+    appendTest(tests, newTestToken(TOKEN_COMMA, ","));
+    appendTest(tests, newTestToken(TOKEN_SEMICOLON, ";"));
 
     Lexer* l = newLexer(input);
 
     for(int i = 0; i < tests->len; i++) {
         Token* tok = nextToken(l);
-        TestToken* tt = tests->arr[i];
-        printf("%d, %s\n", tok->type, tok->literal);
-        printf("%d, %s\n\n", tt->type, tt->literal);
-        free(tok);
+        TestToken* tt = getTestAt(tests, i);
+
+        if(!(tok->type == tt->expectedType)) {
+            printf("tests[%d] tokentype wrong. expected=%d, got=%d.\n", i, tt->expectedType, tok->type);
+            exit(1);
+        }
+
+        if(strcmp(tok->literal, tt->expectedLiteral)) {
+            printf("tests[%d] literal wrong. expected=%s, got = %s.\n", i, tt->expectedLiteral, tok->literal);
+            exit(1);
+        }
     }
+
+    printf("test ok\n");
 }
 
 int main() {
